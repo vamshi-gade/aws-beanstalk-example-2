09f1d23efbcd4d002ed1dcb83a06b26db9667edc
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2021-04-03 09:36:32 +0530

    modified on github

--- src/main/java/com/techprimers/aws/HelloController.java
+++ src/main/java/com/techprimers/aws/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping
     public String hello() {
-        return "remodify on github repo..modify";
+        return "remodify on github repo..new modification";
     }
 }
