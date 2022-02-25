71428e3f70df46c377f16ddfbc58b4938f7d9da4
vamshi-gade <80843582+vamshi-gade@users.noreply.github.com>
2022-02-25 12:28:12 +0530

    test commit for pipeline

--- src/main/java/com/techprimers/aws/HelloController.java
+++ src/main/java/com/techprimers/aws/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping
     public String hello() {
-        return "new change on the repo for testing ";
+        return "new commit for pipeline ";
     }
 }
